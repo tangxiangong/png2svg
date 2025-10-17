053e39fd900ca2eca10770a619c618072f893209
tangxiangong <tangxiangong@gmail.com>
2025-10-17 16:03:22 +0800

    Initialize global Rayon thread pool at module init
    Register a global Rayon thread pool using num_cpus::get for the thread
    count. Add num_cpus to root dependencies and move rayon into workspace
    dependencies, updating crates to reference the workspace rayon

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,11 @@ use pyo3::prelude::*;
 /// import the module.
 #[pymodule]
 fn _core(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(num_cpus::get())
+        .build_global()
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(e.to_string()))?;
+
     m.add_function(wrap_pyfunction!(ffi::convert, m)?)?;
     m.add_function(wrap_pyfunction!(ffi::convert_parallel, m)?)?;
     m.add_function(wrap_pyfunction!(ffi::convert_directory, m)?)?;
